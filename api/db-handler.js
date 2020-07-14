b9699490913d6bea105b2e98ad9726b3cc2c8828
zhiao777774 <47570633+zhiao777774@users.noreply.github.com>
2020-07-15 03:23:30 +0800

    包含與後端進行資料請求之類別

---
+++ api/db-handler.js
@@ -0,0 +1,77 @@
+import io from 'socket.io-client';
+
+class MongoDB {
+    ip = '120.125.83.10';
+    port = '8080';
+
+    constructor() {
+        this.isOpen = false;
+        this.connect();
+    }
+
+    connect(callback) {
+        if (!this.isOpen) {
+            this.socket = io(`http://${this.ip}:${this.port}`);
+            this.isOpen = true;
+
+            if (callback && this._isFunction(callback)) {
+                callback();
+            }
+        }
+    }
+
+    trigger(type, event, query, callback) {
+        this.connect();
+        this.socket.emit(type, Object.assign({}, query, { event }));
+
+        if (callback && this._isFunction(callback)) {
+            this.socket.on(event, callback);
+        }
+    }
+
+    get(query, callback) {
+        this.connect();
+        this._eventRoute('get', query, callback);
+    }
+
+    insert(query, callback) {
+        this.connect();
+        this._eventRoute('insert', query, callback);
+    }
+
+    update(query, callback) {
+        this.connect();
+        this._eventRoute('update', query, callback);
+    }
+
+    delete(query, callback) {
+        this.connect();
+        this._eventRoute('delete', query, callback);
+    }
+
+    close(callback) {
+        if (this.isOpen) {
+            this.socket.close();
+            this.socket = undefined;
+            this.isOpen = false;
+
+            if (callback && this._isFunction(callback)) {
+                callback();
+            }
+        }
+    }
+
+    _eventRoute(event, query, callback) {
+        this.socket.emit(event, query);
+
+        if (callback && this._isFunction(callback)) {
+            this.socket.on(`${event}Result`, callback);
+        }
+    }
+
+    _isFunction(v) {
+        return (typeof v === 'function');
+    }
+}
+
+global.mongoDB = new MongoDB();
