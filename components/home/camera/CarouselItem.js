cd83efc7e65e20f240128c0ecd2924f80a796a0a
zhiao777774 <st1541154190@gmail.com>
2020-07-20 11:07:57 +0800

    加入資料庫圖片格式bson轉base64

--- components/home/camera/CarouselItem.js
+++ components/home/camera/CarouselItem.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { View, StyleSheet, Text, Image, Dimensions } from 'react-native'
+import { Buffer } from 'buffer';
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
 
 const CarouselItem = ({ item }) => {
+    let source;
+    if (item.url) {
+        source = {
+            uri: item.url
+        };
+    } else {
+        const uri = 'data:image/jpg;base64,' + new Buffer(item.image).toString('base64');
+        source = {
+            uri,
+            isStatic: true
+        };
+    }
+
     return (
         <View style={styles.cardView}>
-            <Image style={styles.image} source={{ uri: item.url }} />
+            <Image style={styles.image} source={source} />
             <View style={styles.textView}>
                 <Text style={styles.itemTitle}> {item.title}</Text>
                 <Text style={styles.itemDescription}>{item.description}</Text>
